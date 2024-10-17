7b2447f649512199a1e99471cf04e65728d21f25
Miguel <miguel.dumont@hotmail.com>
2024-10-17 19:03:09 +0200

    bestand tijdelijk.py gecodeerd

---
+++ tijdelijk.py
@@ -0,0 +1,22 @@
+prijzen={
+    "aardbei":3,
+    "Vanille":4,
+    "Chocolade":5
+}
+aanbieding=(prijzen["aardbei"])*0.8
+reclame_tekst=f"Vandaag in de aanbieding: vanille-ijs, 1 liter – slechts € {aanbieding}"
+reclame_tekst2=reclame_tekst[:62]
+reclame_tekst3=reclame_tekst2.upper()
+reclame_tekst4=reclame_tekst3.split()
+for el in reclame_tekst4:
+    
+    if len(el)>5:
+       
+       print(el.upper())
+
+    else:
+
+       print(el.lower())
+   
+
+   
